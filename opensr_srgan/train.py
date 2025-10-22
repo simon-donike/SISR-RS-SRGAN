e80cfc9eb8535867d3025e9046bb2c25f275076f
Simon Donike <accounts@donike.net>
2025-10-22 16:32:01 +0100

    add support for CLI and import training

--- opensr_srgan/train.py
+++ opensr_srgan/train.py
@@ -9,35 +9,27 @@ import wandb
 from omegaconf import OmegaConf
 from pytorch_lightning import Trainer
 
-# set visible GPUs
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-if __name__ == '__main__':
-    import argparse
-    from multiprocessing import freeze_support
-
-    # required for Multiprocessing on Windows
-    freeze_support()
-
-    # ---- CLI: single positional argument (config path) ----
-    parser = argparse.ArgumentParser(description="Train SRGAN with a YAML config.")
-    try:
-        default_config = Path(__file__).resolve().parent / "configs" / "config_10m.yaml"
-    except NameError:
-        default_config = Path.cwd() / "opensr_srgan" / "configs" / "config_10m.yaml"
-    parser.add_argument(
-        "--config", "-c",
-        default=str(default_config),
-        help=f"Path to YAML config file (default: {default_config})"
-    )
-    args = parser.parse_args()
-
-    # General
-    torch.set_float32_matmul_precision('medium')
-    # load config
-    cfg_filepath = args.config
-    config = OmegaConf.load(cfg_filepath)
     
+def train(config):
+    
+    #############################################################################################################
+    """ LOAD CONFIG """
+    # either path to config file or omegaconf object
+    
+    if isinstance(config, str):
+        config = OmegaConf.load(config)    
+    elif isinstance(config, Path):
+        config = str(config)
+        config = OmegaConf.load(config)    
+    elif isinstance(config, dict):
+        config = OmegaConf.create(config)
+    elif isinstance(config, OmegaConf):
+        pass
+    else:
+        raise TypeError("Config must be a filepath (str or Path), dict, or OmegaConf object.")
+    #############################################################################################################
+    
+        
     # Get devices
     cuda_devices = config.Training.gpus
     cuda_strategy = 'ddp' if len(cuda_devices) > 1 else None
@@ -50,7 +42,7 @@ if __name__ == '__main__':
     if config.Model.load_checkpoint != False:
         model = SRGAN_model.load_from_checkpoint(config.Model.load_checkpoint, strict=False)
     else:
-        model = SRGAN_model(config_file_path=cfg_filepath)
+        model = SRGAN_model(config=cfg_filepath)
     if config.Model.continue_training != False:
         resume_from_checkpoint = config.Model.continue_training
     else:
@@ -66,17 +58,25 @@ if __name__ == '__main__':
     #############################################################################################################
     """ Configure Trainer """
     #############################################################################################################
-    # set up logging
-    from pytorch_lightning.loggers import WandbLogger
-    wandb_project = "SRGAN_6bands"   # whatever you want
-    wandb_logger = WandbLogger(
-        project=wandb_project,
-        entity="opensr", 
-        log_model=False
-    )
+    
+    # Configure Logger
+    if config.Logging.wandb.enabled:
+        # set up logging
+        from pytorch_lightning.loggers import WandbLogger
+        wandb_project = config.Logging.wandb.project   # whatever you want
+        wandb_logger = WandbLogger(
+            project=wandb_project,
+            entity=config.Logging.wandb.entity,
+            log_model=False)
+    else:
+        print("Not using Weights & Biases logging, reduced CSV logs written locally.")
+        from pytorch_lightning.loggers import CSVLogger
+        wandb_logger = CSVLogger(
+            save_dir="logs/",)
 
+    # Configure Saving Checkpoints
     from pytorch_lightning.callbacks import ModelCheckpoint
-    dir_save_checkpoints = os.path.join(os.path.normpath("logs/"),wandb_project,
+    dir_save_checkpoints = os.path.join(os.path.normpath("logs/"),config.Logging.wandb.project,
                                                 datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     from opensr_srgan.utils.gpu_rank import _is_global_zero # make dir only on main process
     if _is_global_zero(): # only on main process
@@ -84,7 +84,6 @@ if __name__ == '__main__':
         print("Experiment Path:",dir_save_checkpoints)
         with open(os.path.join(dir_save_checkpoints, "config.yaml"), 'w') as f: # save config to experiment folder
             OmegaConf.save(config, f)
-
     checkpoint_callback = ModelCheckpoint(dirpath=dir_save_checkpoints,
                                             monitor=config.Schedulers.metric,
                                         mode='min',
@@ -120,3 +119,33 @@ if __name__ == '__main__':
     wandb.finish()
 
 
+
+# Run training if called from command line
+if __name__ == '__main__':
+    import argparse
+    from multiprocessing import freeze_support
+
+    # required for Multiprocessing on Windows
+    freeze_support()
+
+    # ---- CLI: single positional argument (config path) ----
+    parser = argparse.ArgumentParser(description="Train SRGAN with a YAML config.")
+    try:
+        default_config = Path(__file__).resolve().parent / "configs" / "config_10m.yaml"
+    except NameError:
+        default_config = Path.cwd() / "opensr_srgan" / "configs" / "config_10m.yaml"
+    parser.add_argument(
+        "--config", "-c",
+        default=str(default_config),
+        help=f"Path to YAML config file (default: {default_config})"
+    )
+    args = parser.parse_args()
+
+    # General
+    torch.set_float32_matmul_precision('medium')
+    # load config
+    cfg_filepath = args.config
+    
+    # Run training
+    train(cfg_filepath)
+    
